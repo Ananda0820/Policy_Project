3c60ddf1d9f784476d23e71527800c74e8b87ea3
Ananda0820 <anandakedlaya@gmail.com>
2024-11-12 17:04:52 +0530

    variables added

--- Demo.java
+++ Demo.java
@@ -2,5 +2,9 @@ class Demo
 {
     public static void main(String[] args) {
         System.out.println("Welcome to github");
+        int a=1;
+        int b=2;
+        int ans=a+b;
+        System.out.println(ans);
     }
 }
